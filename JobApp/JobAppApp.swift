5b2a8f8195f54197d3c2fc5288fe06978045536e
PranavSRokade <pranav.rokade2011@gmail.com>
2026-06-18 12:34:51 +0100

    Initial Commit

---
+++ JobApp/JobAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  JobAppApp.swift
+//  JobApp
+//
+//  Created by Spencer on 18/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct JobAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
